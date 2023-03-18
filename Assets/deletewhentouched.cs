556ab921d55a99f31553a571ee4adfb845f53b24
RafKus0 <rafal.kustra@tm1.edu.pl>
2023-03-18 18:07:44 +0100

    Script

--- Assets/deletewhentouched.cs
+++ Assets/deletewhentouched.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class deletewhentouched : MonoBehaviour
 {
+    public GameObject p3;
+
+    void OnCollisionEnter(Collision col)
+    {
+        {
+            if(col.gameObject.name == "Square")
+            {
+                Debug.Log("Collision detected");
+                p3.GetComponent<Renderer>().enabled = false;
+            }
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
